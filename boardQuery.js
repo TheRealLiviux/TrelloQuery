77a9e9777eb7de6b553a98e53c50d2d1168d544b
Livio Rossani <livio.rossani@interlogica.it>
2020-08-27 09:40:43 +0200

    More statistics

--- boardQuery.js
+++ boardQuery.js
@@ -2,8 +2,9 @@ var fs = require('fs');
 var board = JSON.parse(fs.readFileSync('o0JpJOo6.json', 'utf8'));
 
 // Collect only cards into "Product Backlog"
-var pb = board.lists.filter( l => l.name === "Product Backlog");
-var pbId = pb[0].id;
+var pbId = board.lists.find( l => l.name === "Product Backlog").id;
+var ntbdId = board.lists.find( l => l.name.startsWith("Not")).id;
+
 var productBacklog = board.cards.filter(c => c.idList == pbId && !c.closed);
 console.log("BACKLOG ITEMS:",productBacklog.length);
 
@@ -24,13 +25,17 @@ console.log("UNESTIMATED CARDS:",unestimatedCards.length);
 
 var estimatedCards = productBacklog
 	.filter( c => c.customFieldItems.find( cf => cf.idCustomField == storyPointsId ) )
-	.map( c => [c.idShort, c.name]	);
+	.map( c => [c.idShort, c.name,
+		sp[c.customFieldItems.filter( cf => cf.idCustomField == storyPointsId )[0].idValue]
+		]	);
 console.log("ESTIMATED CARDS:",estimatedCards);
 
+// Total storypoints of all cards (excluding "Not to be done")
 let estimatedTotal=0;
-productBacklog.forEach( function(c) {
-	 let st = c.customFieldItems.find( cf => cf.idCustomField == storyPointsId );
-	 if( st ) estimatedTotal += sp[st.idValue]
-	 }
-	);
+board.cards.forEach( function(c) {
+	if(c.idList != ntbdId) {
+		let st = c.customFieldItems.find( cf => cf.idCustomField == storyPointsId );
+		if( st ) estimatedTotal += sp[st.idValue]
+		}
+	});
 console.log("ESTIMATED STORYPOINTS TOTAL:",estimatedTotal);
